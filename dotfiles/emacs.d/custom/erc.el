976c6530a1039df46ce6f28e981d5d9d281295d0
Steven Spasbo <stevenspasbo@gmail.com>
2014-12-11 13:54:37 -0800

    Fixed username

--- dotfiles/emacs.d/custom/erc.el
+++ dotfiles/emacs.d/custom/erc.el
@@ -1,6 +1,6 @@
 (setq erc-echo-notices-in-minibuffer-flag t)
-(setq erc-keywords '("_sjs_" "steven"))
+(setq erc-keywords '("_sjs" "steven"))
 
 (setq erc-server "weber.freenode.net"
       erc-port 6667
-      erc-nick "_sjs_")
+      erc-nick "_sjs")
