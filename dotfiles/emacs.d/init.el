58207b51254a7d00327be959a72297be32c7ea5d
Steven Spasbo <stevenspasbo@gmail.com>
2014-12-11 13:50:44 -0800

    removed empty custom-set-faces

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -23,15 +23,7 @@
  '(custom-enabled-themes (quote (monokai)))
  '(custom-safe-themes
    (quote
-    ("b6c9e4fe2fe9b9fc799fc2ccae5218e6b9fe23aab9b55efc41ba9e640827d6f4" 
-     "64581032564feda2b5f2cf389018b4b9906d98293d84d84142d90d7986032d33" 
-     "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" 
+    ("b6c9e4fe2fe9b9fc799fc2ccae5218e6b9fe23aab9b55efc41ba9e640827d6f4"
+     "64581032564feda2b5f2cf389018b4b9906d98293d84d84142d90d7986032d33"
+     "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
      "e587bd7ea49915da4556c1f5b535e920cb3f65f033ae636ba8ed0d2ca2a14df4" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-
