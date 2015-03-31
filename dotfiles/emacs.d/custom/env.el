7aa0be9f483c405de665eccd23ce56b32fd01d16
Steven Spasbo <stevenspasbo@gmail.com>
2015-03-30 21:25:48 -0700

    Changed shell, not using old bash...

--- dotfiles/emacs.d/custom/env.el
+++ dotfiles/emacs.d/custom/env.el
@@ -3,11 +3,10 @@
  linum-format "%4d " ;; Add space after linum
  make-backup-files nil ;; Disable backup~
  auto-save-default nil ;; Disable #autosave# files
- shell-file-name "/bin/bash" ;; Sets shell
+ shell-file-name "/usr/local/bin/bash" ;; Sets shell
  inhibit-startup-screen t ;; Skip startup screen
  inferior-lisp-program "/usr/local/bin/sbcl" ;; Sets location of sbcl binary
  confirm-kill-emacs 'y-or-n-p ;; Disallow accidental exits
  user-full-name "Steven Spasbo")
-
 
 (autoload 'neotree-toggle "neotree")
