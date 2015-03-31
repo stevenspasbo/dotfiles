32d0a7c8d05750e241529928217d3ccb942ac14f
Steven Spasbo <stevenspasbo@gmail.com>
2015-03-31 00:37:28 -0700

    Moved custom stuff to custom init

---
+++ dotfiles/emacs.d/init-custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-max-face-count 5))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(calendar-month-header ((t (:inherit font-lock-function-name-face :foreground "#C811ED"))))
+ '(calendar-weekday-header ((t (:inherit font-lock-constant-face :foreground "#874056"))))
+ '(calendar-weekend-header ((t (:inherit font-lock-comment-face :foreground "#FF125C"))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#FFFFFF"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#D60F4D"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#7838ED"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#2899D6"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#00EE4C")))))
