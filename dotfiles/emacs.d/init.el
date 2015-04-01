220c2e788a1db6f77a6f316d4d53bdf0a24aaac4
Steven Spasbo <stevenspasbo@gmail.com>
2015-03-31 18:46:10 -0700

    Load the themes

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,6 +1,6 @@
 ;;;; Author:     Steven Spasbo
 ;;;; Created:    11-30-2014
-;;;; Updated:    03-30-2015
+;;;; Updated:    03-31-2015
 
 (load-file
  (file-truename "~/.emacs.d/elisp/load-directory.el"))
@@ -13,11 +13,14 @@
 (setq custom-file
       (concat emacs-home "/init-custom.el"))
 (load custom-file)
+
 ;; Sets list of directories to load, then iterates over each
 (setq dirs
       '("/custom" "/langs"))
 (dolist (dir dirs)
   (load-directory (concat emacs-home dir)))
+
+(add-to-list 'custom-theme-load-path (concat emacs-home "/themes"))
 
 ;; Load theme
 (load-theme 'hipster t)
