800518a72903146622153cac9f3b391a53ff0516
Steven Spasbo <stevenspasbo@gmail.com>
2015-04-07 12:52:07 -0700

    Generalize lisps file

--- dotfiles/emacs.d/langs/lisps.el
+++ dotfiles/emacs.d/langs/lisps.el
@@ -1,5 +1,20 @@
 ;;; Emacs Lisp
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(defun my/elisp-setup ()
+  (turn-on-eldoc-mode)
+  (local-set-key (kbd "C-c f") 'find-function))
+(add-hook 'emacs-lisp-mode-hook 'my/elisp-setup)
+
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+
+;;; Clojure
+(defun my/clojure-setup()
+  (add-hook 'cider-mode-hook #'eldoc-mode)
+  (setq nrepl-log-messages t)
+  (setq nrepl-hide-special-buffers t))
 
 ;;; Scheme
 (setq scheme-program-name "scheme")
+
+;;; Common Lisp
+'(inferior-lisp-program "/usr/local/bin/sbcl") ; Sets location of sbcl binary
