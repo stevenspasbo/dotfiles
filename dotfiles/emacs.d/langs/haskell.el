b05c76bc5f2b50e528fafb4a261c2ee8ff354300
Steven Spasbo <stevenspasbo@gmail.com>
2015-04-07 12:43:06 -0700

    Haskell stuff

--- dotfiles/emacs.d/langs/haskell.el
+++ dotfiles/emacs.d/langs/haskell.el
@@ -1,2 +1,12 @@
+
+
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(custom-set-variables
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t))
+
+(add-to-list 'completion-ignored-extensions ".hi") ; Ignore compiled files
