bacdcded08dcd9a61ba2af3f89d13e84c53bda49
Steven Spasbo <stevenspasbo@gmail.com>
2015-04-07 11:37:03 -0700

    Added date function, updated doc

--- dotfiles/emacs.d/custom/funcs.el
+++ dotfiles/emacs.d/custom/funcs.el
@@ -1,5 +1,13 @@
+;;;; Emacs functions
 
+;; Reloads init.el
 (defun my/reload-init ()
     (interactive)
     (load-file (concat emacs-home "/init.el")))
 (global-set-key (kbd "C-c r") 'my/reload-init)
+
+(defun date (&optional insert)
+  "Gets the current date and time"
+  (interactive "P")
+  (funcall (if insert 'insert 'message)
+	   (format-time-string "%a, %d %b %Y %r" (current-time))))
