3fadd808a884686fa1dccfa28945e329bc98c0d1
Steven Spasbo <stevenspasbo@gmail.com>
2015-04-07 17:51:34 -0700

    Cleaned up init.el

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -2,25 +2,24 @@
 ;;;; Created:    11-30-2014
 ;;;; Updated:    03-31-2015
 
+;; Sets location of custom file to unclutter init file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;; Enable tab completion
+(setq tab-always-indent 'complete)
+(add-to-list 'completion-styles 'initials t)
+
+;; Loads load-directory function
 (load-file
  (file-truename "~/.emacs.d/elisp/load-directory.el"))
 
-;; Load everything and set the theme
-(defvar emacs-home (file-truename "~/.emacs.d")
-  "Retrieves the full path to my emacs directory")
-
-;; Sets location of custom file to unclutter init file
-(setq custom-file
-      (concat emacs-home "/init-custom.el"))
-(load custom-file)
-
 ;; Sets list of directories to load, then iterates over each
-(setq dirs
-      '("/custom" "/langs"))
+(setq dirs '("custom" "langs"))
 (dolist (dir dirs)
-  (load-directory (concat emacs-home dir)))
+  (load-directory (concat user-emacs-directory dir)))
 
-(add-to-list 'custom-theme-load-path (concat emacs-home "/themes"))
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes"))
 
 ;; Load theme
 (load-theme 'hipster t)
