f72c4f04619704d5529ca3f222ab97265e1c4e7a
Steven Spasbo <stevenspasbo@gmail.com>
2015-10-07 01:03:31 -0700

    Removed duplicate line

--- dotfiles/emacs.d/custom/keys.el
+++ dotfiles/emacs.d/custom/keys.el
@@ -1,7 +1,5 @@
 ;;;; Functions and keybindings
 
-(fset 'yes-or-no-p 'y-or-n-p) ;; Makes yes-or-no questions accept 'y' or 'n'
-
 ;; Enable tab completion
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
