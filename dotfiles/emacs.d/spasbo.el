e7ebd32eff9e8a3f37cc60be4979ef0858e35ed4
Steven Spasbo <stevenspasbo@gmail.com>
2015-10-20 19:13:38 -0700

    Fixing bug with package installs

--- dotfiles/emacs.d/spasbo.el
+++ dotfiles/emacs.d/spasbo.el
@@ -1,11 +1,10 @@
 ;;;; spasbo.el - Load thems, packages, stuff I need before loading files from custom/ directory
 
-(package-initialize)
-
-(require 'cl)
-(require 'package)
 
 (defun spasbo/load ()
+  (require 'cl)
+  (require 'package)
+
   (load-packages)
   (set-theme)
   (set-custom-dir))
@@ -15,70 +14,45 @@
   (load custom-file))
 
 (defun load-packages ()
+  (setq mypackages
+    '( ;; Stand alone
+      rainbow-delimiters rainbow-mode paredit undo-tree helm helm-swoop magit
+                         git-timemachine exec-path-from-shell nyan-mode
+                         android-mode
+                         ;; Modes
+                         racket-mode scheme-complete slime cl-lib haskell-mode
+                         ruby-electric inf-ruby yaml-mode cider clojure-mode
+                         clojure-mode-extra-font-locking hi2 ghc scala-mode2
+                         sbt-mode php-mode vimrc-mode js2-mode ac-js2
+                         ;; Themes
+                         hipster-theme moe-theme zenburn-theme ujelly-theme
+                         tronesque-theme tangotango-theme
+                         color-theme-sanityinc-tomorrow cyberpunk-theme
+                         solarized-theme monokai-theme))
+
   (setq package-archives
         '(("marmalade"   . "http://marmalade-repo.org/packages/")
           ("gnu"         . "http://elpa.gnu.org/packages/")
           ("org"         . "http://orgmode.org/elpa/")
           ("melpa"       . "http://melpa.milkbox.net/packages/")))
 
-  (when (not package-archive-contents)
+  (package-initialize) ; Activates all packages
+
+  (unless package-archive-contents
     (package-refresh-contents))
 
-  (defvar mypackages '(
-                       ;; Stand alone
-                       rainbow-delimiters
-                       rainbow-mode
-                       paredit
-                       undo-tree
-                       helm
-                       helm-swoop
-                       magit
-                       git-timemachine
-                       exec-path-from-shell
-                       nyan-mode
-                       ;; Modes
-                       racket-mode
-                       scheme-complete
-                       slime
-                       cl-lib
-                       haskell-mode
-                       ruby-electric
-                       inf-ruby
-                       yaml-mode
-                       cider
-                       clojure-mode
-                       clojure-mode-extra-font-locking
-                       hi2
-                       ghc
-                       scala-mode2
-                       sbt-mode
-                       php-mode
-                       vimrc-mode
-                       js2-mode
-                       ac-js2
-                       ;; Themes
-                       hipster-theme
-                       moe-theme
-                       zenburn-theme
-                       ujelly-theme
-                       tronesque-theme
-                       tangotango-theme
-                       color-theme-sanityinc-tomorrow
-                       cyberpunk-theme
-                       solarized-theme
-                       monokai-theme
-                       ))
   (dolist (p mypackages)
-    (when (not (package-installed-p p))
+    (message "Checking for %s" p)
+    (unless (package-installed-p p)
+      (message "Installing %s" p)
       (package-install p))))
-
-;; Add custom themes dir
-(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes"))
-
 
 (defun set-theme ()
   "Sets the theme depending on window-system"
   (interactive)
+  ;; Add custom themes dir
+  (add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes"))
+
   (cond ((string-equal window-system "ns")
          (load-theme 'monokai t)
          (nyan-mode))
