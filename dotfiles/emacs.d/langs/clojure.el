9d71c9841cbe3d13c70a6d9b56f695d55f19c967
Steven Spasbo <stevenspasbo@gmail.com>
2016-03-01 13:15:16 -0800

    Updated clojure mode

--- dotfiles/emacs.d/langs/clojure.el
+++ dotfiles/emacs.d/langs/clojure.el
@@ -31,6 +31,26 @@
             (setq nrepl-hide-special-buffers t)))
 
 ;;;; Cider
+
+;; provides minibuffer documentation for the code you're typing into the repl
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+;; go right to the REPL buffer when it's finished connecting
+(setq cider-repl-pop-to-buffer-on-connect t)
+
+;; When there's a cider error, show its buffer and switch to it
+(setq cider-show-error-buffer t)
+(setq cider-auto-select-error-buffer t)
+
+;; Where to store the cider history.
+(setq cider-repl-history-file "~/.emacs.d/cider-history")
+
+;; Wrap when navigating history.
+(setq cider-repl-wrap-history t)
+
+;; enable paredit in your REPL
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
 (defun cider-start-http-server ()
   (interactive)
   (cider-load-current-buffer)
@@ -53,3 +73,7 @@
      (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
      (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
      (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)))
+
+;; (require 'ac-cider)
+;; (add-to-list 'ac-modes 'cider-mode)
+;; (add-to-list 'ac-modes 'cider-repl-mode)
