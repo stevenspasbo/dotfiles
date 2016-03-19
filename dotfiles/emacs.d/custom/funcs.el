645b432f4b7678a2ffd6cd6b1f07ebccec65d3e7
Steven Spasbo <stevenspasbo@gmail.com>
2016-03-18 22:14:55 -0700

    Updated functions
    * Removed unused date function
    * Added rename-file-and-buffer function (credit to Yegge)

--- dotfiles/emacs.d/custom/funcs.el
+++ dotfiles/emacs.d/custom/funcs.el
@@ -6,12 +6,6 @@
     (load-file (concat user-emacs-directory "/init.el")))
 (global-set-key (kbd "C-c r") 'my/reload-init)
 
-(defun date (&optional insert)
-  "Gets the current date and time"
-  (interactive "P")
-  (funcall (if insert 'insert 'message)
-	   (format-time-string "%a, %d %b %Y %r" (current-time))))
-
 ;; Stolen from http://pages.sachachua.com/.emacs.d/Sacha.html#orgheadline131
 (defun sanityinc/kill-back-to-indentation ()
   "Kill from point back to the first non-whitespace character on the line."
@@ -19,3 +13,19 @@
   (let ((prev-pos (point)))
     (back-to-indentation)
     (kill-region (point) prev-pos)))
+
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
