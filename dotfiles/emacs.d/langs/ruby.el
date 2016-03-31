7d7cd1e6368a4f901b640a2d05fac9103ddc8345
Steven Spasbo <stevenspasbo@gmail.com>
2016-03-31 00:31:56 -0700

    Improved ruby mode

--- dotfiles/emacs.d/langs/ruby.el
+++ dotfiles/emacs.d/langs/ruby.el
@@ -1,11 +1,7 @@
-(require 'inf-ruby)
-(require 'yaml-mode)
-(require 'flymake-ruby)
-(require 'robe)
+;;;; ruby.el - Ruby settings
 
 ;; Set file extensions
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
@@ -13,23 +9,27 @@
 
 ;; Function to enable syntax checking, ruby electric, robe and company mode
 (defun ruby-stuff ()
-  (flymake-ruby-load)
-  (ruby-electric-mode t)
-  (robe-mode)
+  (require 'inf-ruby)
+  (require 'robe)
+
+  (setq ruby-indent-level 2
+        ruby-indent-tabs-mode nil
+        ruby-deep-indent-paren nil)
+
+  ;; (ruby-electric-mode t)
+  ;; (robe-mode)
   (push 'company-robe company-backends)
   ;; Stupidly the non-bundled ruby-mode isn't a derived mode of
   ;; prog-mode: we run the latter's hooks anyway in that case.
   (unless (derived-mode-p 'prog-mode)
     (run-hooks 'prog-mode-hook))
-  (setq ruby-deep-indent-paren nil))
+  ;; Launches inf-ruby and starts robe for auto completion
+  (defun run-ruby-and-start-robe ()
+    (interactive)
+    (inf-ruby)
+    (robe-start))
+
+  (global-set-key (kbd "C-c C-e") 'run-ruby-and-start-robe))
 
 ;; Add to ruby-mode-hook
 (add-hook 'ruby-mode-hook 'ruby-stuff)
-
-;; Launches inf-ruby and starts robe for auto completion
-(defun run-ruby-and-start-robe ()
-  (interactive)
-  (inf-ruby)
-  (robe-start))
-
-(global-set-key (kbd "C-c C-e") 'run-ruby-and-start-robe)
