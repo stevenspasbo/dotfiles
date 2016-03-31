b88cdeca54453917908e6cd1fdbb0c3b8ccf0460
Steven Spasbo <stevenspasbo@gmail.com>
2016-03-31 00:29:41 -0700

    Updated haskell config

--- dotfiles/emacs.d/langs/haskell.el
+++ dotfiles/emacs.d/langs/haskell.el
@@ -1,12 +1,13 @@
 
+(defun haskell-mode-stuff ()
+  (turn-on-haskell-doc-mode)
+  (turn-on-haskell-indent)
+  (interactive-haskell-mode)
+  (custom-set-variables
+   '(haskell-process-suggest-remove-import-lines t)
+   '(haskell-process-auto-import-loaded-modules t)
+   '(haskell-process-log t))
+  ;; Ignore compiled files
+  (add-to-list 'completion-ignored-extensions ".hi"))
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-
-(custom-set-variables
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t))
-
-(add-to-list 'completion-ignored-extensions ".hi") ; Ignore compiled files
+(add-hook 'haskell-mode-hook 'haskell-mode-stuff)
