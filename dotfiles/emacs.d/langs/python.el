a9c3bb76d5282ea1ec9d0c7c700d76973cb7a35c
Steven Spasbo <stevenspasbo@gmail.com>
2016-03-31 00:31:09 -0700

    Improved python mode

--- dotfiles/emacs.d/langs/python.el
+++ dotfiles/emacs.d/langs/python.el
@@ -1,4 +1,14 @@
-(setq python-indent 4)
+(require 'elpy)
+(require 'company)
 
-;; Enable elpy
-(elpy-enable)
+(defun python-mode-setup ()
+  (elpy-mode)
+  (when (executable-find "ipython")
+    (elpy-use-ipython))
+  (when (require 'flycheck nil t)
+    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules)))
+
+  (define-key python-mode-map (kbd "RET")
+    'newline-and-indent))
+
+(add-hook 'python-mode-hook 'python-mode-setup)
