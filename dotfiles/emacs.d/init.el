ee2ae9f4f412d9a6081a62e611a7369c68c9f33b
Steven Spasbo <stevenspasbo@gmail.com>
2016-04-01 00:31:03 -0700

    Ugly

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,13 +1,15 @@
-(package-initialize)
-
 ;;;; Author:     Steven Spasbo
 ;;;; Created:    11-30-2014
 ;;;; Updated:    10-18-2015
 
+(package-initialize)
+
 (load-file (expand-file-name "spasbo.el" user-emacs-directory))
+
 (spasbo/load)
 
 ;; Sets list of directories to load, then iterates over each
-(dolist (dir '("custom" "langs"))
-  (load-directory (concat user-emacs-directory dir)))
-
+;; (dolist (dir '("custom" "langs"))
+;;   (load-directory (concat user-emacs-directory dir)))
+(load-directory (concat user-emacs-directory "custom"))
+(load-directory (concat user-emacs-directory "langs"))
