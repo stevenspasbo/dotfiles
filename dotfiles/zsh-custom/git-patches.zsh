73b593e53b0b9594ef0adfd3571c0cdbcb735885
Steven Spasbo <stevenspasbo@gmail.com>
2016-07-22 04:58:19 -0700

    oh-my-zsh patch no longer needed

--- dotfiles/zsh-custom/git-patches.zsh
+++
@@ -1,5 +0,0 @@
-function git_commits_behind() {
-  if git rev-parse --git-dir > /dev/null 2>&1; then
-    echo $(git rev-list --count $(current_branch)..$(git rev-parse --abbrev-ref --symbolic-full-name @{u}))
-  fi
-}
