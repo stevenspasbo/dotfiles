1b0f4aa14ca21449e2f90cc087f140e50cdaf717
Steven Spasbo <stevenspasbo@gmail.com>
2017-04-24 20:50:38 -0700

    zsh - Added keybinding for history substring search previous|next

--- dotfiles/zsh-custom/config/bindkey.zsh
+++ dotfiles/zsh-custom/config/bindkey.zsh
@@ -3,3 +3,6 @@
 autoload run-help # Open man pages with `command <esc-h>`
 
 bindkey '^H' run-help
+
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
