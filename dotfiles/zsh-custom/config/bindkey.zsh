1c2ca055b341f8e87cc908043d79b3aaf568c9cd
Steven Spasbo <stevenspasbo@gmail.com>
2018-04-02 09:33:52 -0700

    zsh keybindings help

--- dotfiles/zsh-custom/config/bindkey.zsh
+++ dotfiles/zsh-custom/config/bindkey.zsh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
-autoload run-help # Open man pages with `command <esc-h>`
-
-bindkey '^H' run-help
+autoload run-help
 
 bindkey -M emacs '^P' history-substring-search-up
 bindkey -M emacs '^N' history-substring-search-down
+
+bindkey "^[h" run-help
+bindkey "^Xb" describe-key-briefly
